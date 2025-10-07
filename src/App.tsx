633a4a95db5ec48682bc7a0214ec74903febd408
Coded_mind__ <shahzadekhan296@gmail.com>
2025-10-07 19:21:03 +0530

    File manager 95% complete just close btn and btn desing left

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ import { Button } from '@mantine/core'
 
 function App() {
   const [Window, setWindow] = useState(false)
+
+     
+
+
   useEffect(() => {
     console.log(Window)
   }, [Window])
@@ -23,6 +27,7 @@ function App() {
           <BarBox />
           <div style={{ display: "flex ", width: "100%", flexDirection: "column" }}>
             <TopBox />
+            
             <AppWindow WindowState={Window} setWindow={setWindow} />
             <Button onClick={() => { Window ? setWindow(false) : setWindow(true) }} >Click</Button>
             <GlassCard links={[{ link: "/icons/folderWhite.svg", bg: "#c01d1c", hovertext: "Projects" }, { link: "icons/terminal.svg", bg: "black", hovertext: "Terminal" }, { link: "icons/notes.png", bg: "#eed509b8", hovertext: "Notes" }, { link: "/icons/CodedMind2.png", bg: "black", hovertext: "About me" }, { link: "/icons/photos.png", bg: "white", hovertext: "Photos" }, { link: "/icons/settings.png", bg: "#3d403f", hovertext: "Settings" }, { link: "/icons/github.png", bg: "black", hovertext: "Github" }]} />
--- src/component/AppWindow.tsx
+++ src/component/AppWindow.tsx
@@ -55,16 +55,16 @@ function AppWindow({ WindowState, setWindow }: AppWindowProps) {
                 topRight: true,
                 bottomLeft: true,
                 bottomRight: true
-            }} >
-                <div style={{ border: "solid red", height: "100%" }} className="bg-black/90 rounded-md  text-white shadow-lg flex-col flex  ">
-                    <div style={{ border: "solid red" }} className="TopHead flex flex-row items-center justify-center">
-                        <h1 className="font-bold relative left-[9%]">Hii</h1>
+            }} cancel=".content" >
+                <div style={{height: "100%" }} className="MainAppContainer rounded-md  text-white shadow-lg flex-col flex  ">
+                    <div  className="rounded-t-md TopHead  flex flex-row items-center justify-center">
+                        <h1 className="font-bold relative left-[9%]">File</h1>
                         <div className="relative left-[40%]">
-                            <Button onClick={() => setWindow(!WindowState)}>Minimize</Button>
-                            <Button >Click</Button>
+                            {/* <Button onClick={() => setWindow(!WindowState)}>Minimize</Button>
+                            <Button >Click</Button> */}
                         </div>
                     </div>
-                    <div  className="h-[100%]">
+                    <div  className="content h-[100%]">
                         <FileManager cssClass="my-bark-file-manager" files={files} />
                     </div>
                 </div>
